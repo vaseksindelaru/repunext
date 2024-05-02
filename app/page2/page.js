ddfefe66ee77717da7c0e337cc684962bf0ed42a
vaseksindelaru <vaseksindelaru@gmail.com>
2024-05-02 22:32:43 +0200

    grid carteluo

--- app/page2/page.js
+++ app/page2/page.js
@@ -12,18 +12,19 @@ return {data}
     const tarea = await loadTareas()
     console.log(tarea)
 
-    return(
-      <div className="grid gap-4 grid-cols-5">   
+    return(<div><h1 className='text-center font-mono text-3xl text-yelow text-yellow-600 font-bold  mb-8'>mis notas</h1>
+      <div className="mx-12 grid gap-4 grid-cols-5">   
        
            {tarea.data[0].map((tarea)=>(
-  <div key={tarea.id} className="bg-white rounded-lg border-gray-800 mb-3 text-center ">
-    <p className='text-xl font-bold m-1'>{tarea.tema}</p>
-    <p className='text-lg text-slate-600'>{tarea.subtema}</p>
-    <p className='text-base text-slate-300'>{tarea.subtema}</p>
-   
+  <div key={tarea.id} className="bg-orange-500 p-4 rounded-lg border-gray-800 mb-2 text-center">
+    <div className=" p-4 ">
+    <p className='text-xl font-bold m-1 font-mono'>{tarea.tema}</p>
+    <p className='text-base text-slate-600 m-1'>{tarea.subtema}</p>
+    <p className='text-sm text-slate-300 m-1'>{tarea.subtema}</p>
+   </div>
   </div>
  ))}
-      </div>  )
+      </div></div>  )
   }
 export default TareasList
 
